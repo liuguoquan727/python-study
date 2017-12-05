ba46424322ec9ecc962a65f384f2a163bd89810a
刘国权 <liuguoquan727@126.com>
2017-12-05 20:52:27 +0800

    [python]-tensorflow init

---
+++ tensorflow/init.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+*
+* Description:
+*
+* Created by liuguoquan on 2017/11/22 11:40
+*
+"""
+
+import tensorflow as tf
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a + b))
+sess.close()
